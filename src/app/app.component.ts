bdfff03ee90f8a3c3f6874e57b2376483864dcaf
jacquelineatae <dev.atae02@gmail.com>
2024-01-24 08:52:10 -0300

    Creating the component base

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuTitleComponent } from './components/menu-title/menu-title.component';
+import { SmallCardComponent } from './components/small-card/small-card.component'
+import { BigCardComponent } from './components/big-card/big-card.component'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenuTitleComponent, SmallCardComponent, BigCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
