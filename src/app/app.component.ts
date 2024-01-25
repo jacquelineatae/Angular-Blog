7843c3ab1a08327a2b475c578b846e62106e5db8
jacquelineatae <dev.atae02@gmail.com>
2024-01-25 16:21:50 -0300

    Structuring the big card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MenuTitleComponent } from './components/menu-title/menu-title.component';
-import { SmallCardComponent } from './components/small-card/small-card.component'
-import { BigCardComponent } from './components/big-card/big-card.component'
+import { HomeComponent } from './pages/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MenuTitleComponent, SmallCardComponent, BigCardComponent],
+  imports: [RouterOutlet, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
