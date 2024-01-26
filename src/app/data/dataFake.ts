606743e5de679aa8663a2c0c7807544c66e140e3
jacquelineatae <dev.atae02@gmail.com>
2024-01-26 12:33:21 -0300

    Making the content page dynamic

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,20 +1,26 @@
 export const dataFake = [
   {
-    "id":1,
-    "photo": "aaa",
-    "title": "bbb",
-    "description": "ccc"
+    "id":"1",
+    "title": "Teste Big Card",
+    "description": "Funcionou!!",
+    "photoCover":"https://deepgram.com/_next/image?url=https%3A%2F%2Fwww.datocms-assets.com%2F96965%2F1684215758-800x0-2.webp&w=1920&q=75"
   },
   {
-    "id":3,
-    "photo": "aaa",
-    "title": "bbb",
-    "description": "ccc"
+    "id":"2",
+    "title": "Teste Small Card UM",
+    "description": "Uhuu",
+    "photoCover":"https://deepgram.com/_next/image?url=https%3A%2F%2Fwww.datocms-assets.com%2F96965%2F1684215758-800x0-2.webp&w=1920&q=75"
   },
   {
-    "id":3,
-    "photo": "aaa",
-    "title": "bbb",
-    "description": "ccc"
+    "id":"3",
+    "title": "Teste Small Card DOIS",
+    "description": "Tomaa",
+    "photoCover":"https://deepgram.com/_next/image?url=https%3A%2F%2Fwww.datocms-assets.com%2F96965%2F1684215758-800x0-2.webp&w=1920&q=75"
+  },
+  {
+    "id":"4",
+    "title": "Teste Small Card TRÊS",
+    "description": "fOII",
+    "photoCover":"https://deepgram.com/_next/image?url=https%3A%2F%2Fwww.datocms-assets.com%2F96965%2F1684215758-800x0-2.webp&w=1920&q=75"
   },
 ];
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input} from '@angular/core';
-import { RouterLink } from '@angular/router';
-
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { dataFake } from '../../data/dataFake'
 @Component({
   selector: 'app-content',
   standalone: true,
@@ -15,9 +15,28 @@ export class ContentComponent {
   contentTitle:string = ""
   @Input()
   contentDescription:string = ""
+  private id:string | null= "0"
 
-  constructor(){}
+  constructor(
+    private rout:ActivatedRoute
+  ){}
 
   ngOnInit(): void {
+    this.rout.paramMap.subscribe( value =>
+      this.id = value.get("id")
+    )
+
+    this.setValuesToComponent(this.id)
   }
+
+  setValuesToComponent(id:string | null){
+    const result = dataFake.filter(article => article.id.toString() == id)[0]
+
+
+    this.contentTitle = result.title
+    this.contentDescription = result.description
+    this.photoCover = result.photoCover
+
+  }
+
 }
