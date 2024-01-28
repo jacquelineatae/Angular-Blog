5d2a7a94b5a104693fb23bffa2497ff0ebfddfa9
jacquelineatae <dev.atae02@gmail.com>
2024-01-28 20:23:11 -0300

    Some changes

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,8 +1,8 @@
 export const dataFake = [
   {
     "id":"1",
-    "title": "Teste Big Card",
-    "description": "Funcionou!!",
+    "title": "Orcas: como elas se comunicam?",
+    "description": "As orcas, Orcinus orca, conhecidas popularmente como ‘baleias-assassinas’, são mamíferos marinhos pertencentes à superfamília Odontoceti, grupo representado, também, pelos golfinhos, botos e cachalotes. Com a superfamília Mysticeti, representada pelas grandes baleias, formam a infra-ordem Cetacea. Esses animais são conhecidos pela sua comunicação vocal complexa. Você consegue imaginar por que isso ocorre? Vamos descobrir!  ",
     "photoCover":"https://deepgram.com/_next/image?url=https%3A%2F%2Fwww.datocms-assets.com%2F96965%2F1684215758-800x0-2.webp&w=1920&q=75"
   },
   {
